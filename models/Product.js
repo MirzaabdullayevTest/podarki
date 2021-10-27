89f5a313e1bf0b527d99a8003ff756a98f2c78e4
MirzaabdullayevTest <mirzaabdullayevtest@mail.ru>
2021-10-27 22:39:07 +0500

    podarki

---
+++ models/Product.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+
+const product = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  info: Array,
+});
+
+module.exports = model("product", product);
